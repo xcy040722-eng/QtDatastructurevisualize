093e693cb068ad357a9f21798a5d3386bd029b3e
yuyu <xcy040722@gmail.com>
2025-11-29 00:22:15 +0800

    增加了鼠标左键拖动

--- QtDatastructurevisualize/mainwindow.cpp
+++ QtDatastructurevisualize/mainwindow.cpp
@@ -27,8 +27,11 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     // 2. 背景色调整
     m_view->setBackgroundBrush(Qt::lightGray);
 
-    // 3. 移除 ScrollHandDrag，使用默认指针
-    m_view->setDragMode(QGraphicsView::NoDrag);
+    // 3. 开启拖拽模式 (ScrollHandDrag)
+    // 这样鼠标会变成一只“手”，按住左键即可拖动画布
+    m_view->setDragMode(QGraphicsView::ScrollHandDrag);
+
+    // 4. 自动滚动条策略 (当拖到边缘或缩放过大时出现)
     m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
     m_view->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
 
